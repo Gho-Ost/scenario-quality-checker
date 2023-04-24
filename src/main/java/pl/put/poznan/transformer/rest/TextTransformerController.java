b9ede9680ff58b504f4faa521eaf2118acc18af0
Gho-Ost <100212265+Gho-Ost@users.noreply.github.com>
2023-04-24 09:20:30 +0200

    Add project framework

---
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -0,0 +1,46 @@
+package pl.put.poznan.transformer.rest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.logic.TextTransformer;
+
+import java.util.Arrays;
+
+
+@RestController
+@RequestMapping("/{text}")
+public class TextTransformerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public String get(@PathVariable String text,
+                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        TextTransformer transformer = new TextTransformer(transforms);
+        return transformer.transform(text);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public String post(@PathVariable String text,
+                      @RequestBody String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        TextTransformer transformer = new TextTransformer(transforms);
+        return transformer.transform(text);
+    }
+
+
+
+}
+
+
