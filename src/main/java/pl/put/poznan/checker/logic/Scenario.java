6fbee11b27f3ab88fd35e6d08d2edb760c58e5c0
barte <bartek.pukacki@gmail.com>
2023-04-30 22:58:21 +0200

    tagging test

--- src/main/java/pl/put/poznan/checker/logic/Scenario.java
+++ src/main/java/pl/put/poznan/checker/logic/Scenario.java
@@ -1,6 +1,9 @@
 package pl.put.poznan.checker.logic;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Scenario implements Visitable{
     private String title;
@@ -8,14 +11,28 @@ public class Scenario implements Visitable{
     private String systemActor;
 
     // TEST steps as list of step objects idk if this would work
-    private ArrayList<Step> steps;
+    private ArrayList<Object> steps;
 
-    public Scenario(String title, String[] actors, String systemActor, ArrayList<Step> steps) {
+    public Scenario(String title, String[] actors, String systemActor, ArrayList<Object> steps) {
         this.title = title;
         this.actors = actors;
         this.systemActor = systemActor;
         this.steps = steps;
     }
+
+    public void accept(Visitor visitor) {
+        //TODO: Implement further logic here
+        visitor.visit(this);
+    }
+
+    public ArrayList<Object> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<Object> steps) {
+        this.steps = steps;
+    }
+
 
     public String getTitle() {
         return title;
@@ -39,19 +56,5 @@ public class Scenario implements Visitable{
 
     public void setSystemActor(String systemActor) {
         this.systemActor = systemActor;
-    }
-
-    public ArrayList<Step> getSteps() {
-        return steps;
-    }
-
-    public void setSteps(ArrayList<Step> steps) {
-        this.steps = steps;
-    }
-
-
-    public void accept(Visitor visitor) {
-        //TODO: Implement further logic here
-        visitor.visit(this);
     }
 }
