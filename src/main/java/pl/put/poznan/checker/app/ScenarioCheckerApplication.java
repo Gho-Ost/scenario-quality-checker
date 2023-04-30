487f60d84aeb3a82549b1c814a0ee8274a7a4890
barte <bartek.pukacki@gmail.com>
2023-04-30 16:17:26 +0200

    Updated naming (Transformer->Checker)

---
+++ src/main/java/pl/put/poznan/checker/app/ScenarioCheckerApplication.java
@@ -0,0 +1,13 @@
+package pl.put.poznan.checker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.checker.rest"})
+public class ScenarioCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScenarioCheckerApplication.class, args);
+    }
+}
