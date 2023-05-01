53017fb7ad0e000c8dbb46c77d4402cc0db8fd84
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-05-01 18:40:21 +0200

    Implement getting scenario up-to-level

--- src/main/java/pl/put/poznan/checker/logic/ScenarioLevelVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/ScenarioLevelVisitor.java
@@ -1,9 +1,13 @@
 package pl.put.poznan.checker.logic;
 
+import java.util.ArrayList;
+
+//TODO: Test and review this implementation
 public class ScenarioLevelVisitor implements Visitor{
     private int currentLevel = 1;
     private int maxLevel=1;
     private Scenario scenario;
+    private ArrayList<Step> steps;
 
     public void setMaxLevel(int maxLevel){
         this.maxLevel=maxLevel;
@@ -16,9 +20,16 @@ public class ScenarioLevelVisitor implements Visitor{
     }
 
     public void visit(Step step){
+        if(this.currentLevel<=this.maxLevel){
+            if(this.currentLevel+1>this.maxLevel){
+                step.setSubsteps(null);
+            }
+            this.steps.add(step);
+        }
     }
 
     public Scenario getScenario(){
+        scenario.setSteps(this.steps);
         return scenario;
     }
 
