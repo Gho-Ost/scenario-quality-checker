dfc5ef1ed92356de3fc71e3931bb35c64e4de711
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-05-01 17:05:08 +0200

    Change actor detection to be consistent with requirements

--- src/main/java/pl/put/poznan/checker/logic/ScenarioMissingActorVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/ScenarioMissingActorVisitor.java
@@ -6,26 +6,18 @@ import java.util.List;
 public class ScenarioMissingActorVisitor {
     public class NoActorStepVisitor implements Visitor {
         private List<Step> noActorSteps = new ArrayList<>();
-        private List<Integer> missingActorsIndexes = new ArrayList<>();
-        private int currentIndex=0;
 
         public void visit(Scenario scenario) {
         }
 
         public void visit(Step step) {
-            currentIndex+=1;
             if (step.getActor() == null) {
                 noActorSteps.add(step);
-                missingActorsIndexes.add(currentIndex);
             }
         }
 
         public List<Step> getNoActorSteps() {
             return noActorSteps;
-        }
-
-        public List<Integer> getMissingActorsIndexes(){
-            return missingActorsIndexes;
         }
     }
 }
