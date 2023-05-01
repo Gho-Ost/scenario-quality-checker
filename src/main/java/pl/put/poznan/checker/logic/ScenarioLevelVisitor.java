d91077e315d46613556bc8cd119a1b069ac2e613
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-05-01 18:05:13 +0200

    Fix bug involving class declaration

---
+++ src/main/java/pl/put/poznan/checker/logic/ScenarioLevelVisitor.java
@@ -0,0 +1,2 @@
+package pl.put.poznan.checker.logic;public class ScenarioLevelVisitor {
+}
--- src/main/java/pl/put/poznan/checker/logic/ScenarioMissingActorVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/ScenarioMissingActorVisitor.java
@@ -3,21 +3,22 @@ package pl.put.poznan.checker.logic;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScenarioMissingActorVisitor {
-    public class NoActorStepVisitor implements Visitor {
-        private List<Step> noActorSteps = new ArrayList<>();
+public class ScenarioMissingActorVisitor implements Visitor{
+    private List<Step> noActorSteps = new ArrayList<>();
 
-        public void visit(Scenario scenario) {
-        }
+    public void visit(Scenario scenario) {}
 
-        public void visit(Step step) {
-            if (step.getActor() == null) {
-                noActorSteps.add(step);
-            }
-        }
-
-        public List<Step> getNoActorSteps() {
-            return noActorSteps;
+    public void visit(Step step) {
+        if (step.getActor() == null) {
+            noActorSteps.add(step);
         }
     }
+
+    public List<Step> getNoActorSteps() {
+        return noActorSteps;
+    }
+
+    public void incrementDepth(){}
+
+    public void decrementDepth(){}
 }
