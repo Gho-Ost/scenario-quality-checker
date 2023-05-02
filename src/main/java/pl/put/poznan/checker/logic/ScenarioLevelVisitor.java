99f9719515cd745eb36e774637d55daa2a84bcc9
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-05-02 14:26:28 +0200

    Fix bug related to step-level

--- src/main/java/pl/put/poznan/checker/logic/ScenarioLevelVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/ScenarioLevelVisitor.java
@@ -24,7 +24,9 @@ public class ScenarioLevelVisitor implements Visitor{
             if(currentLevel+1>this.maxLevel){
                 step.setSubsteps(null);
             }
-            this.steps.add(step);
+            if(currentLevel==1) {
+                this.steps.add(step);
+            }
         }
     }
 
