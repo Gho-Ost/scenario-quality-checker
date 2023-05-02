39a3dfc11f97f7aa1d526d20824ccf5b701a314b
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-05-02 18:04:51 +0200

    Add local download functionality

--- src/main/java/pl/put/poznan/checker/logic/ScenarioTextDownloadVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/ScenarioTextDownloadVisitor.java
@@ -3,12 +3,35 @@ package pl.put.poznan.checker.logic;
 import pl.put.poznan.checker.model.Scenario;
 import pl.put.poznan.checker.model.Step;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 //TODO change to new step level format
 public class ScenarioTextDownloadVisitor implements Visitor {
     private StringBuilder result = new StringBuilder();
+    private String downloadPath;
+
+    public ScenarioTextDownloadVisitor(){
+        String homeDir = System.getProperty("user.home");
+        Path downloadsDir = Paths.get(homeDir, "Downloads");
+        Path filePath = downloadsDir.resolve("Scenario.txt");
+        downloadPath = filePath.toString();
+    }
 
     public String getResult() {
         return result.toString();
+    }
+
+    public void setDownloadPath(String downloadPath){
+        this.downloadPath=downloadPath;
+    }
+
+    public void download() throws IOException {
+        Path path = Paths.get(downloadPath);
+        byte[] bytes = result.toString().getBytes();
+        Files.write(path, bytes);
     }
 
     @Override
